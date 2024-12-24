3fedae9326a44f214d34e3b9780f2f4b7e082cf2
aliofye <ooo@ali.ooo>
2024-12-24 15:30:48 -0500

    Set hono port manually

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -4,4 +4,7 @@ const app = new Hono();
 app.get('/', (c) => c.text('A-ok!'));
 app.get('/message', (c) => c.json({ message: 'Hello, World!' }));
 
-export default app;
+export default {
+  port: Bun.env.PUBLIC_API_PORT || 3000,
+  fetch: app.fetch,
+};
