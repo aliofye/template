d7420303866cced95ed12daee6eafe936008370a
aliofye <ooo@ali.ooo>
2025-01-01 18:55:35 -0500

    Added docker logo

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -3,6 +3,7 @@ import viteLogo from '/vite.svg';
 import bunLogo from '/bun.svg';
 import honoLogo from '/hono.svg';
 import lefthookLogo from '/lefthook.svg';
+import dockerLogo from '/docker.svg';
 import classes from './index.module.css';
 import classNames from '../helpers/classNames';
 
@@ -13,6 +14,13 @@ const Home = () => {
     <div className={classes.container}>
       <Navbar />
       <div className={classes.logoContainer}>
+        <a href="https://evilmartians.com/opensource/lefthook" target="_blank">
+          <img
+            src={lefthookLogo}
+            className={classNames(classes.logo, classes.lefthook)}
+            alt="Lefthook logo"
+          />
+        </a>
         <a href="https://react.dev" target="_blank">
           <img
             src={reactLogo}
@@ -27,13 +35,6 @@ const Home = () => {
             alt="Vite logo"
           />
         </a>
-        <a href="https://bun.sh" target="_blank">
-          <img
-            src={bunLogo}
-            className={classNames(classes.logo, classes.bun)}
-            alt="Bun logo"
-          />
-        </a>
         <a href="https://hono.dev" target="_blank">
           <img
             src={honoLogo}
@@ -41,11 +42,18 @@ const Home = () => {
             alt="Hono logo"
           />
         </a>
-        <a href="https://evilmartians.com/opensource/lefthook" target="_blank">
+        <a href="https://bun.sh" target="_blank">
           <img
-            src={lefthookLogo}
-            className={classNames(classes.logo, classes.lefthook)}
-            alt="Lefthook logo"
+            src={bunLogo}
+            className={classNames(classes.logo, classes.bun)}
+            alt="Bun logo"
+          />
+        </a>
+        <a href="https://docs.docker.com/" target="_blank">
+          <img
+            src={dockerLogo}
+            className={classNames(classes.logo, classes.docker)}
+            alt="Docker logo"
           />
         </a>
       </div>
