01f2583eaed0c55da375567cc118743d325b066f
aliofye <ooo@ali.ooo>
2025-01-02 15:55:40 -0500

    Added stylelint

--- packages/web/src/components/Navbar/Navbar.tsx
+++ packages/web/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css';
+import classes from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav>
-      <div className={styles.navLinks}>
+      <div className={classes.navLinks}>
         <Link to="/">Home</Link>
         <Link to="/example">Example</Link>
         <Link to="/examples/slug">Slug Example</Link>
