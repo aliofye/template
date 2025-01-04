7079082f318d3e90002622bedc4b480e3eaf8565
aliofye <ooo@ali.ooo>
2025-01-04 02:45:12 -0500

    Add linting fix script, update stylelint configuration, and enhance CSS styles for better layout consistency

--- packages/web/stylelint.config.js
+++ packages/web/stylelint.config.js
@@ -1,12 +1,8 @@
 export default {
   ignoreFiles: ['**/node_modules/**', '**/dist/**'],
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
-  plugins: ['stylelint-order'],
+  extends: ['stylelint-config-clean-order'],
+  plugins: ['stylelint-prettier'],
   rules: {
-    'color-no-invalid-hex': true,
-    'font-family-no-missing-generic-family-keyword': true,
-    'order/properties-alphabetical-order': true,
-    'no-empty-source': null,
-    'selector-class-pattern': null,
+    'prettier/prettier': true,
   },
 };
