db85baea558709d3a8618114f957c019b040d257
aliofye <ooo@ali.ooo>
2025-01-10 23:26:44 -0500

    Added MDX support for documentation

--- packages/web/src/pages/Docs/index.tsx
+++ packages/web/src/pages/Docs/index.tsx
@@ -10,63 +10,64 @@ import lefthookLogo from '../../assets/lefthook.svg';
 import dockerLogo from '../../assets/docker.svg';
 
 import ExampleNavbar from '../../components/ExampleNavbar';
+import Markdown from './Docs.mdx';
 
 const Docs = () => {
   return (
     <div>
       <ExampleNavbar />
-      <div className="container">
-        <section>
-          <div className={classes.logoContainer}>
-            <a href="https://evilmartians.com/opensource/lefthook" target="_blank">
-              <img
-                src={lefthookLogo}
-                className={classNames(classes.logo, classes.lefthook)}
-                alt="Lefthook logo"
-              />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img
-                src={reactLogo}
-                className={classNames(classes.logo)}
-                alt="React logo"
-              />
-            </a>
-            <a href="https://vite.dev" target="_blank">
-              <img
-                src={viteLogo}
-                className={classNames(classes.logo, classes.vite)}
-                alt="Vite logo"
-              />
-            </a>
-            <a href="https://hono.dev" target="_blank">
-              <img
-                src={honoLogo}
-                className={classNames(classes.logo, classes.hono)}
-                alt="Hono logo"
-              />
-            </a>
-            <a href="https://bun.sh" target="_blank">
-              <img
-                src={bunLogo}
-                className={classNames(classes.logo, classes.bun)}
-                alt="Bun logo"
-              />
-            </a>
-            <a href="https://docs.docker.com/" target="_blank">
-              <img
-                src={dockerLogo}
-                className={classNames(classes.logo, classes.docker)}
-                alt="Docker logo"
-              />
-            </a>
-          </div>
+      <div className={classes.scrollContainer}>
+        <section className={classes.logoContainer}>
+          <a href="https://evilmartians.com/opensource/lefthook" target="_blank">
+            <img
+              src={lefthookLogo}
+              className={classNames(classes.logo, classes.lefthook)}
+              alt="Lefthook logo"
+            />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img
+              src={reactLogo}
+              className={classNames(classes.logo)}
+              alt="React logo"
+            />
+          </a>
+          <a href="https://vite.dev" target="_blank">
+            <img
+              src={viteLogo}
+              className={classNames(classes.logo, classes.vite)}
+              alt="Vite logo"
+            />
+          </a>
+          <a href="https://hono.dev" target="_blank">
+            <img
+              src={honoLogo}
+              className={classNames(classes.logo, classes.hono)}
+              alt="Hono logo"
+            />
+          </a>
+          <a href="https://bun.sh" target="_blank">
+            <img
+              src={bunLogo}
+              className={classNames(classes.logo, classes.bun)}
+              alt="Bun logo"
+            />
+          </a>
+          <a href="https://docs.docker.com/" target="_blank">
+            <img
+              src={dockerLogo}
+              className={classNames(classes.logo, classes.docker)}
+              alt="Docker logo"
+            />
+          </a>
           <h1> = ❤️</h1>
           <p className={classes.readTheDocs}>
             Click on any of the logos to read their documentation
           </p>
         </section>
-        <section></section>
+        <section className={classes.docsContainer}>
+          <Markdown />
+        </section>
       </div>
     </div>
   );
