4186131089c97e8359f277baa742a92ca06e4bd9
aliofye <ooo@ali.ooo>
2025-01-16 17:40:12 -0500

    Leverage react router fs route conventions

--- packages/web/app/routes/examples.$slug.tsx
+++ packages/web/app/routes/examples.$slug.tsx
@@ -1,23 +1,19 @@
 import { useParams } from 'react-router';
-import ExampleNavbar from '@/web/components/ExampleNavbar';
 
 const SlugExample = () => {
   const { slug } = useParams<{ slug: string }>();
 
   return (
-    <div>
-      <ExampleNavbar />
-      <div className="content">
-        <h1>Slug Example Page</h1>
-        <p>
-          This is an example page using slug:
-          <span className="gradient"> {slug}</span>.
-        </p>
-        <p className="gradient fade-transition">
-          Try to adjust the url from <code>/slug-example/slug</code> to{' '}
-          <code>/slug-example/hello</code>
-        </p>
-      </div>
+    <div className="content">
+      <h1>Slug Example Page</h1>
+      <p>
+        This is an example page using slug:
+        <span className="gradient"> {slug}</span>.
+      </p>
+      <p className="gradient fade-transition">
+        Try to adjust the url from <code>/slug-example/slug</code> to{' '}
+        <code>/slug-example/hello</code>
+      </p>
     </div>
   );
 };
