f3b8ea59fb4ab599d417f4394f5556928177dd81
aliofye <ooo@ali.ooo>
2025-01-16 16:36:32 -0500

    Fixed prod build issues

--- packages/web/app/root.tsx
+++ packages/web/app/root.tsx
@@ -12,16 +12,6 @@ import stylesheet from './app.css?url';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
   { rel: 'stylesheet', href: stylesheet },
 ];
 
@@ -50,7 +40,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <div id="root">{children}</div>
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
